11d8c05468f91b9031fc5fd2697830dbbc942bd2
Anurag Keshri <anuragkeshri47@gmail.com>
2024-10-15 16:16:45 +0530

    feat: update build script and improve express configuration

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(auth);
 
